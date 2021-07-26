c4b9f853c346df27cc17dcfa20f7aafe05cb11e9
x-idssh <87958653+x-idssh@users.noreply.github.com>
2021-07-26 21:58:38 +0700

    Update supprot.sh

--- supprot.sh
+++ supprot.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-apt install -y bzip2 gzip coreutils curl unzip netcat tcpd libsctp-dev cmake make gcc zlib1g-dev libwrap0-dev certbot perl libperl-dev libgd3 libgd-dev libgeoip1 libgeoip-dev geoip-bin libxml2 libxml2-dev libxslt1.1 libxslt1-dev build-essential git tree libpcre3-dev socket dh-autoreconf libssh-4 libssh-dev libconfig9 lolcat mercurial screen chron
+apt install -y bzip2 gzip iptables-persistent nftables coreutils curl unzip netcat tcpd libsctp-dev cmake make gcc zlib1g-dev libwrap0-dev certbot perl libperl-dev libgd3 libgd-dev libgeoip1 libgeoip-dev geoip-bin libxml2 libxml2-dev libxslt1.1 libxslt1-dev build-essential git tree libpcre3-dev socket dh-autoreconf libssh-4 libssh-dev libconfig9 lolcat mercurial screen chron
+# Create and Configure rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+exit 0
+END
+# Disable check Certificate
+echo "check_certificate = off" >> ~/.wgetrc
+
+# Iptables Nat
+sudo nft add table nat
+sudo nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
+sudo nft 'add chain nat prerouting { type nat hook prerouting priority -100; }'
+sudo nft add rule nat postrouting masquerade
