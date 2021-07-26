f64d9e5b95758023aabdc9c9fcd03e580a043a14
x-idssh <87958653+x-idssh@users.noreply.github.com>
2021-07-27 02:19:13 +0700

    Add files via upload

--- deb10.sh
+++ deb10.sh
@@ -43,34 +43,6 @@ read -p "Hostname / Domain: " host
 echo "$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /root/domain
 echo -e ""
-echo -e "========================="
-echo -e "| Installing Ruby . . . |"
-echo -e "-------------------------"
-echo -e ""
-apt-get install ruby -y
-echo -e ""
-
-# Checking Ruby Version
-echo -e "==============================="
-echo -e "| Checking Ruby Version . . . |"
-echo -e "-------------------------------"
-echo -e ""
-ruby --version
-echo -e ""
-
-# Download & Install Lolcat
-echo -e "==================================="
-echo -e "| Download & Install Lolcat . . . |"
-echo -e "-----------------------------------"
-echo -e ""
-wget https://github.com/x-idssh/xxx/raw/main/master.zip
-unzip master.zip
-cd lolcat-master/bin
-gem install lolcat
-echo -e ""
-echo "-----------=[ X-IDSSH ]=-----------" | lolcat
-echo -e ""
-
 # Confirm to Install Script
 read -p "Do you want to install this script ? [Y/n] " Answer
 echo -e ""
@@ -89,58 +61,6 @@ wget https://raw.githubusercontent.com/x-idssh/zxf/main/nginx.sh && chmod +x ngi
 wget https://raw.githubusercontent.com/demonk1992/x/main/openssl.sh && chmod +x openssl.sh && sed -i -e 's/\r$//' openssl.sh && screen -S openssl ./openssl.sh
 # Install OpenVPN
 wget https://raw.githubusercontent.com/x-idssh/zxf/main/vpn.sh && chmod +x vpn.sh && sed -i -e 's/\r$//' vpn.sh && screen -S vpn ./vpn.sh
-# Detail Company Profile
-country=ID
-state=Jawa-Barat
-locality=Subabg
-organization=X-IDSSH
-organizationalunit=IDSSH.XYZ
-commonname=IDSSH
-email=x-idssh@gmail.com
-# Common Password
-git clone https://github.com/x-idssh/xxx
-cd /root/xxx/common
-mv common /etc/pam.d/
-chmod +x /etc/pam.d/common-password
-# Edit file /etc/systemd/system/rc-local.service
-cat > /etc/systemd/system/rc-local.service <<-END
-[Unit]
-Description=/etc/rc.local
-ConditionPathExists=/etc/rc.local
-[Service]
-Type=forking
-ExecStart=/etc/rc.local start
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-SysVStartPriority=99
-[Install]
-WantedBy=multi-user.target
-END
-# Enable rc local
-systemctl enable rc-local
-systemctl start rc-local.service
-# remove unnecessary files
-apt -y autoremove
-apt -y autoclean
-apt -y clean
-# set time GMT +7
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-# set locale
-sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
-# install udpgw
-wget https://raw.githubusercontent.com/x-idssh/zxf/main/udpgw.sh && chmod +x udpgw.sh && sed -i -e 's/\r$//' udpgw.sh && screen -S udpgw ./udpgw.sh
-# Sett Port OpenSSH
-sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
-service ssh restart
-# Install Dropbear
-apt-get -y install dropbear
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 222"/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
-/etc/init.d/dropbear restart
 # Install Squid Proxy
 wget https://raw.githubusercontent.com/x-idssh/zxf/main/squid.sh && chmod +x squid.sh && sed -i -e 's/\r$//' squid.sh && screen -S squid ./squid.sh
 # Setting Vnstat
@@ -153,40 +73,6 @@ wget https://raw.githubusercontent.com/demonk1992/x/main/sslh.sh && chmod +x ssl
 wget https://raw.githubusercontent.com/demonk1992/x/main/stunnel.sh && chmod +x stunnel.sh && sed -i -e 's/\r$//' stunnel.sh && screen -S stunnel ./stunnel.sh
 # ssh-websocket
 wget https://raw.githubusercontent.com/demonk1992/down/main/websocket.sh && chmod +x websocket.sh && sed -i -e 's/\r$//' websocket.sh && screen -S websocket ./websocket.sh
-# Install Fail2ban
-cdLEDinstall fail2ban
-apt-get -y install fail2ban
-# Instal DDOS Flate
-if [ -d '/usr/local/ddos' ]; then
-	echo; echo; echo "Please un-install the previous version first"
-	exit 0
-else
-	mkdir /usr/local/ddos
-fi
-echo; echo 'Installing DOS-Deflate 0.6'; echo
-echo; echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
-echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
-echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
-echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
-chmod 0755 /usr/local/ddos/ddos.sh
-cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
-echo '...done'
-echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
-/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
-echo '.....done'
-echo; echo 'Installation has completed.'
-echo 'Config file is at /usr/local/ddos/ddos.conf'
-echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
-# Add Banner
-cat > /etc/idssh.net <<-END
-<h6><font color='blue'>IDSSH.XYZ</font></h6>
-END
-sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
-sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 # Install Menu
 cd /usr/bin 
 wget -O mem-tr "https://raw.githubusercontent.com/x-idssh/zxf/main/mem-tr.sh"
@@ -211,22 +97,39 @@ wget -O clear-log "https://raw.githubusercontent.com/demonk1992/down/main/clear-
 echo "0 5 * * * root clear-log && reboot" >> /etc/crontab
 
 chmod +x mem-tr
+sed -i -e 's/\r$//' mem-tr
 chmod +x menu
+sed -i -e 's/\r$//' menu
 chmod +x usernew
+sed -i -e 's/\r$//' usernew
 chmod +x trial
+sed -i -e 's/\r$//' trial
 chmod +x deluser
+sed -i -e 's/\r$//' deluser
 chmod +x member
+sed -i -e 's/\r$//' member
 chmod +x delete
+sed -i -e 's/\r$//' delete
 chmod +x cek
+sed -i -e 's/\r$//' cek
 chmod +x restart
+sed -i -e 's/\r$//' restart
 chmod +x speedtest
+sed -i -e 's/\r$//' speedtest
 chmod +x info
+sed -i -e 's/\r$//' info
 chmod +x about
+sed -i -e 's/\r$//' about
 chmod +x autokill
+sed -i -e 's/\r$//' autokill
 chmod +x tendang
+sed -i -e 's/\r$//' tendang
 chmod +x ceklim
+sed -i -e 's/\r$//' ceklim
 chmod +x ram
+sed -i -e 's/\r$//' ram
 chmod +x renew
+sed -i -e 's/\r$//' renew
 chmod +x clear-log
 # finishing
 cd
