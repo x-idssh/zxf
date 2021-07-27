4176a3d7df1e512bb4692deb5953a13e321c3cf0
x-idssh <87958653+x-idssh@users.noreply.github.com>
2021-07-27 10:23:12 +0700

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MYIP=$(wget -qO- icanhazip.com);
+MYIP=$(curl -s http://whatismyip.akamai.com/);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 apt-get -y install squid3
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/x-idssh/zxf/main/squid3.conf"
