e4b3c48aa1b0411ac8711f04521cc8a906e7743e
x-idssh <87958653+x-idssh@users.noreply.github.com>
2021-07-27 15:26:31 +0700

    Delete squid.sh

--- squid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-MYIP=$(curl -s http://whatismyip.akamai.com/);
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-apt-get -y install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/x-idssh/zxf/main/squid3.conf"
-sed -i $MYIP2 /etc/squid/squid.conf
-service squid restart
